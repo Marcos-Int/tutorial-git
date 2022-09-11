4dc5fc9e8c7f9fcd889b9e189c4a5f732be5cebe
Marcos Henrique <olivermhos23@gmail.com>
2022-09-11 01:10:34 -0300

    If ternário e Switch case em Javascript

---
+++ ifTernario.js
@@ -0,0 +1,22 @@
+// IF ternário 
+var x = 140;
+var y = 12; 
+
+let resultado = (x > y) ? `${x} é maior que ${y}` : `${y} é maior que ${x}`;
+
+console.log(resultado);
+
+// SWITCH
+let dia = 'domingo';
+switch (dia){
+    case 'domingo':
+        console.log('Que bom dia de praia!');
+    break;
+    default:
+        console.log('Não é dia de praia!');
+}
+
+const a = 15;
+const b = 6;
+
+console.log(a > b ? `${a} é maior que ${b}` : `${b} é maior que ${a}`);
