974415b58432f85321ddb02223ea4c1d5f0a3689
Marcos Henrique <olivermhos23@gmail.com>
2022-09-13 08:03:44 -0300

    Inclusão de um novo FOR para identificar pares e ímpares

--- forTeste.js
+++ forTeste.js
@@ -1,5 +1,21 @@
+console.log('=============================================');
+console.log('    ============   TABUADA   ============    ');
+console.log('=============================================');
+
 let tabuada = 10;
 
 for(let i = 0; i <= 10; i++){
     console.log(`${tabuada} x ${i} = ${tabuada * i}`);
+}
+
+console.log('============================================');
+console.log('== IDENTIFICANDO SE NÚMERO É PAR OU ÍMPAR ==')
+console.log('============================================');
+
+for(let i = 0; i <= 10; i++){
+    if(i % 2 == 0){
+        console.log(`${i} é PAR`);
+    } else {
+        console.log(`${i} é ÍMPAR`);
+    }
 }
